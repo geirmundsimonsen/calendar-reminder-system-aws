d7e99dfc8066d14791cb2eb5aa796ae2a58af81a
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2021-06-30 09:24:05 +0200

    add todo notifications

--- cal-rem-lambda/src/notifier.rs
+++ cal-rem-lambda/src/notifier.rs
@@ -1,10 +1,12 @@
+use chrono::prelude::*;
 use lambda_runtime::Error;
+use rand::{Rng, SeedableRng, rngs::SmallRng, seq::SliceRandom};
 use std::env::var;
 use crate::matrix::Matrix;
-use crate::parser::get_calendar_entries_from_file;
 use crate::notify::{create_notifications_from_calendar, get_notifications_within_time_window};
+use crate::parser::get_calendar_entries_from_file;
 use crate::s3::{get_object_as_string, save_string_as_object};
-use chrono::prelude::*;
+use crate::todo::get_todo_entries_from_aws;
 
 pub async fn run_notifier() -> Result<(), Error> {
     let now = Utc::now().timestamp();
@@ -18,19 +20,28 @@ pub async fn run_notifier() -> Result<(), Error> {
     let notifications = create_notifications_from_calendar(&get_calendar_entries_from_file().await?);
     let notifications_within_time_window = get_notifications_within_time_window(&notifications, Utc::now().timestamp(), previous_now);
     
-    let messages: Vec<String> = notifications_within_time_window.iter().map(|notification| {
+    let mut messages: Vec<String> = notifications_within_time_window.iter().map(|notification| {
         notification.msg.clone()
     }).collect();
 
+    {
+        let mut rng = SmallRng::from_entropy();
+        let now = Utc::now();
+        if now.hour() > 8 && now.hour() < 23 && rng.gen::<f64>() < 1.0/60.0 {
+            let mut todo_entries = get_todo_entries_from_aws().await?;
+            todo_entries.shuffle(&mut rng);
+            todo_entries.first().map(|entry| messages.push(entry.clone()));
+        }
+    }
     
     if messages.len() > 0 {
         println!("{:?}", messages);
         Matrix { server: var("MATRIX_SERVER")? }.authenticate_and_send_messages_to_room(
-        &var("MATRIX_USER")?,
-        &var("MATRIX_PW")?,
-        &var("MATRIX_REMINDER_ROOM")?,
-        messages
-    ).await;
+            &var("MATRIX_USER")?,
+            &var("MATRIX_PW")?,
+            &var("MATRIX_REMINDER_ROOM")?,
+            messages
+        ).await;
     }
 
     save_string_as_object(now.to_string(), var("S3_MAIN_BUCKET")?, "notification-last-run-timestamp.txt".to_string()).await?;
